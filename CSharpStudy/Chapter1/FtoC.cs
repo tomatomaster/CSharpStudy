f6e887f363d57721fb26173e1b35e08d24fe30cf
tomatomaster <iiitomatomasteriii@gmail.com>
2017-10-22 23:41:17 +0900

    First Commit

---
+++ CSharpStudy/Chapter1/FtoC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharpStudy
+{
+    public class FtoC
+    {
+        static void Main(){
+            double f;
+            double c;
+
+            f = 59.0;
+            c = 5.0 / 9.0 * (f - 32.0);
+
+            Console.Write(f + " degrees Fahrenheit is ");
+            Console.Write(c + " degrees Celsius.");
+        }
+    }
+}
